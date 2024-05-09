fb99fb918270c296a3ec59bf56c0486bc7088810
BOOPESH-foxy <boopesh.mc@gmail.com>
2024-05-09 17:06:41 +0530

    updated vpc

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "state-host"
-#     key    = "terraform-state/terraform.tfstate"
-#     region = "ap-south-1"
-#     dynamodb_table = "terraform_locks"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "state-host"
+    key    = "terraform-state/terraform.tfstate"
+    region = "ap-south-1"
+    dynamodb_table = "terraform_locks"
+  }
+}
 
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
